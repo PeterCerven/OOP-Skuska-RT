fbaa4d8289e7644c30f6fd6659978c5982ef049f
Jakub Kovář <jakub.kovar@stuba.sk>
2021-06-16 17:20:36 +0200

    renamed main class

--- src/main/java/sk/stuba/fei/uim/oop/ExamRT.java
+++ src/main/java/sk/stuba/fei/uim/oop/ExamRT.java
@@ -1,7 +1,7 @@
 package sk.stuba.fei.uim.oop;
 import sk.stuba.fei.uim.oop.rt.Paint;
 
-public class Assignment2 {
+public class ExamRT {
     public static void main(String[] args) {
         new Paint();
     }
